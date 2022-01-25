743407cfec927504858e18594ab64d69cedbb773
Ola Chrzan <ola.chrzan16@gmail.com>
2022-01-25 13:51:29 +0100

    Add colors and breakpoints to theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,18 @@
 export const theme = {
-  
+  colors: {
+    white: "#FFFFFF",
+    mainBackground: "#F5F5FA",
+    headerBackground: "#18181B",
+    darkText: "#18181B",
+    grayText: "#7E839A",
+    grayButton: "#E4E6F0",
+    lightBlueButton: "#D6E4FF",
+    darkBlueButton: "#0044CC",
+  },
+  breakpoints: {
+    small: "480px",
+    medium: "767px",
+    large: "992px",
+    extraLarge: "1280px",
+  }
 }
