93e63a89582855c5fa265fa46892c0f6fee0a22a
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-01-27 22:50:25 +0100

    change apiUrl

--- src/features/movies/MoviesList/movieListSaga.js
+++ src/features/movies/MoviesList/movieListSaga.js
@@ -1,14 +1,19 @@
 import { takeLatest, call, put, delay } from "redux-saga/effects";
-import { getApi } from "../../../getApi";
-import { fetchMovies, setMovies, setError, setTotalPages } from "./movieListSlice";
+import { getApi } from "../../getApi";
+import {
+    fetchMovies,
+    setMovies,
+    setError,
+    setTotalPages
+} from "./movieListSlice";
+import { apiUrlPopularMovies, apiKey } from "../../apiData";
 
 function* fetchMovieListHandler() {
-    const apiUrl = "https://api.themoviedb.org/3/movie/popular?api_key=65f0040ec23653aeaba018e96e0fe430";
+    const apiUrl = `${apiUrlPopularMovies}${apiKey}`;
 
     try {
         yield delay(2000);
         const apiRequest = yield call(getApi, apiUrl);
-        console.log(apiRequest);
         yield put(setMovies(apiRequest.results));
         yield put(setTotalPages(apiRequest.total_pages));
     }
