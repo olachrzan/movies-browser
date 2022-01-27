0ba03136ae19866b3b00989908c0d35f1c753bd7
Ola Chrzan <ola.chrzan16@gmail.com>
2022-01-27 12:54:07 +0100

    Add media query to MoviesList

--- src/features/movies/MoviesList/styled.js
+++ src/features/movies/MoviesList/styled.js
@@ -6,4 +6,9 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
   gap: 24px;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+    gap: 16px;
+  }
 `;
