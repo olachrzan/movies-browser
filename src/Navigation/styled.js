75eda518459d6592f37c945ce2a75b3ce35f745a
Gosia_Ras <ras.malgorzata@gmail.com>
2022-01-28 16:54:42 +0100

    code cleaned

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -26,18 +26,11 @@ export const Icon = styled.img`
 
 export const Title = styled.h1`
   margin: 0;
-  font-weight: 400;
+  font-weight: 500;
   font-size: 24px;
   letter-spacing: -1.5px;
   display: flex;
   align-items: center;
-  flex-shrink: 0;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
-    flex-basis: 495px;
-    justify-content: space-between;
-    flex-shrink: 1;
-  }
 `;
 
 export const TextSide = styled.div`
