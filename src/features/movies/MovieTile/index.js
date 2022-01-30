bf0ce525234ca6109c5546f31f4c054a8768ad68
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-01-30 23:10:10 +0100

    download data from API

---
+++ src/features/movies/MovieTile/index.js
@@ -0,0 +1,45 @@
+import { useSelector } from "react-redux";
+import { selectGenres } from "../MoviesList/movieListSlice";
+import {
+  Poster,
+  Rating,
+  RatingLine,
+  Star,
+  Tag,
+  TagsLine,
+  TextSide,
+  Title,
+  Votes,
+  Wrapper,
+  Year
+} from "./styled";
+
+export const MovieTile = ({ poster, title, year, rating, voteCount, genres }) => {
+  const genresList = useSelector(selectGenres);
+
+  return (
+    <Wrapper>
+      <Poster src={poster} alt="poster" />
+      <TextSide>
+        <Title>{title}</Title>
+        <Year>{year}</Year>
+        {genresList.length > 0 &&
+          <TagsLine>
+            {genres.map((genre, id) => (
+              <Tag key={id}
+              >
+                {[...genresList].find((item) => item.id === genre).name}
+              </Tag>
+            ))
+            }
+          </TagsLine>
+        }
+        <RatingLine>
+          <Star />
+          <Rating>{rating}</Rating>
+          <Votes> <span>{voteCount}</span> votes</Votes>
+        </RatingLine>
+      </TextSide>
+    </Wrapper >
+  );
+}
