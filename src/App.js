5378b5da1dea1b9b28da82b9926b25d842bd7ec7
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-01-30 13:50:06 +0100

    temporary changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Container from "./common/Container";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchMovies } from "./features/movies/MoviesList/movieListSlice";
+import { fetchMovieDetails } from "./features/movies/movieDetailsSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMovies())
+    dispatch(fetchMovies());
+    dispatch(fetchMovieDetails());
   }, [dispatch]);
 
   return (
