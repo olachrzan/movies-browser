d10fe290eae29dba5e29076fac86451575605d13
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-03 18:42:08 +0100

    added id for movie

--- src/features/movies/MoviesList/index.js
+++ src/features/movies/MoviesList/index.js
@@ -1,11 +1,12 @@
 import { MovieTile } from "../MovieTile"
 import { Wrapper } from "./styled";
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { selectMovies } from "./movieListSlice";
 import { apiUrlImage } from "../../apiData";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import { fetchMovies } from "../MoviesList/movieListSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import { StyledLink } from "../wrapperLink";
 
 export const MovieList = () => {
   const dispatch = useDispatch();
@@ -17,16 +18,18 @@ export const MovieList = () => {
 
   return (
     <Wrapper>
-      {[...movies].map((movie, index) => {
-        return <MovieTile key={index}
-          poster={`${apiUrlImage}w500/${movie.poster_path}`} //here we need to change the image for mobile phones 
-          title={movie.title}
-          year={(movie.release_date).slice(0, 4)}
-          genres={movie.genre_ids}
-          rating={movie.vote_average}
-          voteCount={movie.vote_count}
-        />
+      {[...movies].map((movie) => {
+        return <StyledLink key={nanoid()} to={`/movie/${movie.id}`} >
+          <MovieTile
+            poster={`${apiUrlImage}w500/${movie.poster_path}`} //here we need to change the image for mobile phones 
+            title={movie.title}
+            year={(movie.release_date).slice(0, 4)}
+            genres={movie.genre_ids}
+            rating={movie.vote_average}
+            voteCount={movie.vote_count}
+          />
+        </StyledLink>
       })}
-    </Wrapper>
+    </Wrapper >
   )
 };
