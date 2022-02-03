e3c3cdd52518b5397f984fc1548a2fc92e643278
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-03 18:39:15 +0100

    added movieDetailsSaga to rootSaga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { MovieDetailsPage } from "./features/movies/MovieDetails/MovieDetailsPage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import MovieDetailsPage from "./features/movies/MovieDetails/MovieDetailsPage";
+import { MovieList } from "./features/movies/MoviesList";
 
 function App() {
 
   return (
-      <MovieDetailsPage />
+    <HashRouter>
+      <Switch>
+        <Route path="/movie/:id" >
+          <MovieDetailsPage />
+        </Route>
+        <Route path="/movie">
+          <MovieList />
+        </Route >
+        <Route path="/">
+          <Redirect to="/movie" />
+        </Route>
+      </Switch>
+    </HashRouter >
   );
 };
 
