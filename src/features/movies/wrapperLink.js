368d4ea1d97c565235c13af58fb3091d62477c1b
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-05 14:52:44 +0100

    added hover

--- src/features/movies/wrapperLink.js
+++ src/features/movies/wrapperLink.js
@@ -4,4 +4,9 @@ import { Link } from "react-router-dom";
 export const StyledLink = styled(Link)`
     text-decoration: none;
     color: ${({ theme }) => theme.colors.darkText};
+    transition: transform .4s;
+
+    &:hover{
+        transform: scale(1.04);
+    }
 `;
