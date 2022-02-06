3dabb6d74e78c6c2093ed49e169998c0fcae22d1
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-06 17:19:47 +0100

    moved FetchSearchMovies hook to getApi.js

--- src/features/Search/SearchPage.js
+++ src/features/Search/SearchPage.js
@@ -1,23 +1,19 @@
 import { Search } from "./index";
 import { MovieList } from "../movies/MovieList";
+import { FetchSearchMovies } from "../getApi";
 
 export const SearchPage = () => {
   const apiUrlSearchMovies =
     "https://api.themoviedb.org/3/search/movie?&api_key=5d668a4e55e28292714d6fdd2fd67935&query=";
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [moviesSearch, setMoviesSearch] = useState([]);
+ 
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
     if (searchTerm) {
-      fetch(apiUrlSearchMovies + searchTerm)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          setMoviesSearch(data.results);
-        });
+      FetchSearchMovies();
       setSearchTerm("");
     }
   };
--- src/features/getApi.js
+++ src/features/getApi.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import { apiUrlSearchMovies } from "./apiData";
+import { useState } from "react";
 
 export const getApi = async (apiUrl) => {
-    const response = await axios.get(apiUrl);
-    
-    return await response.data;
+  const response = await axios.get(apiUrl);
+
+  return await response.data;
+};
+
+export const FetchSearchMovies = async (query, id) => {
+  const [moviesSearch, setMoviesSearch] = useState([]);
+  const response = await fetch(apiUrlSearchMovies + id + query);
+  const data = response.json();
+  if (response.status > 400) {
+    throw new Error(data.error);
+  }
+
+  return setMoviesSearch(data.results);
 };
