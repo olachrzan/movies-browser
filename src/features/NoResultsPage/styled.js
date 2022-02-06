b57c2995bb5fa97ae382aeeffb0bb417cab6ba52
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-06 11:43:12 +0100

    adding title to no results page

--- src/features/NoResultsPage/styled.js
+++ src/features/NoResultsPage/styled.js
@@ -1,18 +1,5 @@
 import styled from "styled-components";
 
-export const NoResultsText = styled.p`
-  font-weight: 600;
-  font-size: 33px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
-    font-size: 30px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
-    font-size: 22px;
-  }
-`;
-
 export const Image = styled.img`
   display: block;
   margin: 0 auto;
