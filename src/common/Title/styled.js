6e57d10c2d8bc98afc349216dbc692632f6c79ab
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-07 21:08:47 +0100

    added title to common

---
+++ src/common/Title/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 24px;
+  font-weight: 600;
+  font-size: 36px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.larger}) {
+    font-size: 30px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    margin-bottom: 12px;
+    font-size: 18px;
+  }
+`;
