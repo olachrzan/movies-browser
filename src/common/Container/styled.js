99f057deef87db1d86bad1c85db9d7c2fd92898b
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-07 21:08:18 +0100

    added breakpoint

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,4 +4,8 @@ export const Wrapper = styled.div`
     max-width: 1408px;
     padding: 0px 20px;
     margin: 0 auto;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+        padding: 0px 16px;
+    }
 `;
