700c9802719ee87f048ec5fbdd83b52b539fbc20
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-08 19:21:21 +0100

    created Wrapper for Cast and Crew

---
+++ src/features/movies/MovieDetails/Wrapper.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(208px, 1fr)); 
+    grid-gap: 24px;
+    @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+        grid-gap: 16px;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+        grid-template-columns: 1fr 1fr;
+    }
+`;
