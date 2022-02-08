d1a66e96337420c5b501f97594d1f1ac00dd5723
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-02-08 19:19:11 +0100

    added style for moviePage

--- src/common/Title/styled.js
+++ src/common/Title/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Title = styled.h2`
   margin-top: 0;
@@ -12,4 +12,14 @@ export const Title = styled.h2`
     margin-bottom: 12px;
     font-size: 18px;
   }
+
+  ${({ movie }) => movie && css`
+    font-size: 36px;
+    margin-bottom: 32px;
+
+    @media(max-width:${({ theme }) => theme.breakpoints.container}px){
+      font-size: calc(20px + (36 - 20) * ((100vw - 320px) / (1368 - 320)));
+      margin-top: calc(21px + (64 - 21) * ((100vw - 320px) / (1368 - 320)));
+    }
+  `}
 `;
