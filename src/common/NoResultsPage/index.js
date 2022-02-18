47f8c83084ca803452abec9793aa855eec882612
Gosia_Ras <ras.malgorzata@gmail.com>
2022-02-18 10:59:06 +0100

    NoResultsPage

--- src/common/NoResultsPage/index.js
+++ src/common/NoResultsPage/index.js
@@ -1,12 +1,14 @@
 import image from "../NoResultsPage/icons/no-results.svg";
 import { Image } from "./styled";
-import Container from "../../common/Container";
-import { Title } from "../../common/Title/styled";
+import { Container } from "../../common/Container";
+import { Title } from "../Tile/styled";
+import { useQueryParameter } from "../useQueryParameter";
 
-export const NoResultsPage = ({ title }) => {
+export const NoResultsPage = () => {
+  const search = useQueryParameter("search");
   return (
     <Container>
-      <Title>Sorry, there are no results for "{title}"</Title>
+      <Title>Sorry, there are no results for "{search}"</Title>
       <Image src={image} />
     </Container>
   );
--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,12 +1,18 @@
 import { Pagination } from "../../../common/Pagination";
 import { Container } from "../../../common/Container";
-import { Tile } from "../../../common/Tile"
+import { Tile } from "../../../common/Tile";
 import { Wrapper } from "./styled";
 import { useSelector } from "react-redux";
 import { apiUrlImage } from "../../apiData";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { fetchMovies, selectLoading, selectError, selectMovies,selectTotalResults } from "./movieListSlice";
+import {
+  fetchMovies,
+  selectLoading,
+  selectError,
+  selectMovies,
+  selectTotalResults,
+} from "./movieListSlice";
 import { Title } from "../../../common/Title/styled";
 import { WrapperLink } from "../../../common/wrapperLink";
 import { nanoid } from "@reduxjs/toolkit";
@@ -14,6 +20,8 @@ import { Loader } from "../../../common/Loader";
 import { ErrorPage } from "../../../common/ErrorPage";
 import posterError from "../../../images/posterError.png";
 import { useLocation } from "react-router";
+import { NoResultsPage } from "../../../common/NoResultsPage";
+/* import { useQueryParameter } from "../../../common/useQueryParameter"; */
 
 export const MovieList = () => {
   const dispatch = useDispatch();
@@ -21,9 +29,11 @@ export const MovieList = () => {
   const error = useSelector(selectError);
   const location = useLocation();
   const totalResults = useSelector(selectTotalResults);
-  const query = (new URLSearchParams(location.search)).get("search");
+  const query = new URLSearchParams(location.search).get("search");
 
   const movies = useSelector(selectMovies);
+
+ /*  const search = useQueryParameter("search") */
 
   useEffect(() => {
     dispatch(fetchMovies({ query }));
@@ -32,38 +42,48 @@ export const MovieList = () => {
   return (
     <Container>
       <Title>
-        {`${query
-          ? `Search results for "${query.toUpperCase()}" (${totalResults})`
-          : "Popular movies"}`}
+        {`${
+          query
+            ? `Search results for "${query.toUpperCase()}" (${totalResults})`
+            : "Popular movies" 
+        }`}
       </Title>
-      {loading
-        ? <Loader />
-        : error ? <ErrorPage />
-          : (
-            <>
-              <Wrapper>
-                {[...movies].map((movie) => {
-                  return <WrapperLink key={nanoid()} to={`/movie/${movie.id}`} >
-                    <Tile
-                      poster={
-                        movie.poster_path
-                          ? `${apiUrlImage}w500/${movie.poster_path}` //here we need to change the image for mobile phones
-                          : posterError
-                      }
-                      title={movie.title}
-                      year={movie.release_date ? (movie.release_date).slice(0, 4) : "year: unknown"}
-                      genres={movie.genre_ids}
-                      rating={movie.vote_average}
-                      voteCount={movie.vote_count}
-                      overview={movie.overview}
-                    />
-                  </WrapperLink>
-                })}
-              </Wrapper>
-              <Pagination />
-            </>
-          )
-      }
-    </Container >
-  )
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <ErrorPage />
+      ) : movies.length === 0 ? (
+        <NoResultsPage />
+      ) : (
+        <>
+          <Wrapper>
+            {[...movies].map((movie) => {
+              return (
+                <WrapperLink key={nanoid()} to={`/movie/${movie.id}`}>
+                  <Tile
+                    poster={
+                      movie.poster_path
+                        ? `${apiUrlImage}w500/${movie.poster_path}` //here we need to change the image for mobile phones
+                        : posterError
+                    }
+                    title={movie.title}
+                    year={
+                      movie.release_date
+                        ? movie.release_date.slice(0, 4)
+                        : "year: unknown"
+                    }
+                    genres={movie.genre_ids}
+                    rating={movie.vote_average}
+                    voteCount={movie.vote_count}
+                    overview={movie.overview}
+                  />
+                </WrapperLink>
+              );
+            })}
+          </Wrapper>
+          <Pagination />
+        </>
+      )}
+    </Container>
+  );
 };
