c6419a462eea050bc2fab8f4100ea6083d5273a6
Ola Chrzan <ola.chrzan16@gmail.com>
2022-02-20 12:27:18 +0100

    Add error message for invalid page number in url

--- src/common/ErrorPage/index.js
+++ src/common/ErrorPage/index.js
@@ -1,13 +1,28 @@
 import { ErrorContainer, ErrorImage, ErrorMessage, HomepageButton } from "./styled";
 import image from "./icons/error.svg";
+import { useQueryParameter } from "../../queryParameters";
+import { useSelector } from "react-redux";
+import { selectTotalMoviesPages } from "../../features/movies/MovieList/movieListSlice";
+import { selectPeopleTotalPage } from "../../features/people/peopleList/peopleListSlice";
+import { useLocation } from "react-router";
 
 export const ErrorPage = () => {
+  const page = useQueryParameter("page");
+  const {pathname} = useLocation();
+  const totalPage = useSelector(
+    pathname === "/movie" ? selectTotalMoviesPages : selectPeopleTotalPage
+  );
+  const totalCurrentPage = totalPage > 500 ? 500 : totalPage;
+
   return (
     <ErrorContainer>
-      <ErrorImage src={image}/>
+      <ErrorImage src={image} />
       <ErrorMessage>Ooops, something went wrong...</ErrorMessage>
       <ErrorMessage smaller>
-        Please check your network connection and try again
+        {page <= 0 || page > totalCurrentPage
+          ? "It looks like the page you're looking for doesn't exist"
+          : "Please check your network connection and try again"
+        }
       </ErrorMessage>
       <HomepageButton to={"/"}>Back to homepage</HomepageButton>
     </ErrorContainer>
