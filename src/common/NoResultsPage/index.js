f07930ae29ef93ad82d9f9225fba633b68e5ead9
Ola Chrzan <ola.chrzan16@gmail.com>
2022-02-20 22:13:51 +0100

    Fix problem with no query

--- src/common/NoResultsPage/index.js
+++ src/common/NoResultsPage/index.js
@@ -2,14 +2,20 @@ import image from "../NoResultsPage/icons/no-results.svg";
 import { Image } from "./styled";
 import { Title } from "../../common/Title/styled";
 import { useQueryParameter } from "../../queryParameters";
+import { Loader } from "../Loader";
 
 export const NoResultsPage = () => {
   const query = useQueryParameter("search");
 
   return (
-    <>
-      <Title>Sorry, there are no results for "{query[0].toUpperCase()}{query.slice(1)}"</Title>
-      <Image src={image} />
-    </>
+    !query || query === 0
+      ? <Loader />
+      :
+      <>
+        <Title>
+          Sorry, there are no results for "{query[0].toUpperCase()}{query.slice(1)}"
+        </Title>
+        <Image src={image} />
+      </>
   );
 };
